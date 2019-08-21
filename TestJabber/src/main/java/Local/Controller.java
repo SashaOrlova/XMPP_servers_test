dc4024b242674a8bce3bbe2eeb468fa048639910
Александра Орлова <aleksandra.orlova@192-168-0-100.local>
2019-08-21 06:41:00 +0300

    refactor

---
+++ TestJabber/src/main/java/Local/Controller.java
@@ -0,0 +1,164 @@
+package Local;
+
+import Local.Comunicator.ClientCommunicator;
+import Local.Configuration.ConfigParser;
+import Local.Configuration.MainConfig;
+import Local.UI.Interpreter;
+import Local.UI.Mode;
+import Local.UI.Plot;
+
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class Controller {
+    private int mode = Mode.ONLINE;
+    private MainConfig config;
+    private ClientCommunicator communicator = new ClientCommunicator();
+    private static final String FILE_NAME = "result";
+
+    /**
+     * connect to clients
+     * @throws IOException
+     */
+    public void start() throws IOException {
+        if (config == null) {
+            config = ConfigParser.readConfig();
+        }
+
+        if (!checkConfig(config)) {
+            Interpreter.reportAboutError("wrong config");
+        }
+        communicator.startCommunication(config);
+    }
+
+    /**
+     * loaf config into clients instance
+     * @throws IOException
+     */
+    public void loadConfig() throws IOException {
+        communicator.sendConfig();
+    }
+
+    /**
+     * parse config from command line
+     */
+    public void parseConfig() {
+        Scanner in = new Scanner(System.in);
+
+        config = ConfigParser.readConfig(in);
+    }
+
+    /**
+     * launch tests on clients
+     *
+     * @throws IOException
+     */
+    public void startTest() throws IOException {
+        Queue<Long> globalQueue = new ConcurrentLinkedQueue<>();
+        communicator.startTesting(globalQueue);
+        if (!checkConfig(config)) {
+            Interpreter.reportAboutError("wrong config");
+        }
+        if (mode == Mode.ONLINE) {
+            Thread ui = new Plot(config, globalQueue);
+            ui.start();
+        } else {
+            waitUntilEnd(globalQueue, config);
+            saveInFile(globalQueue);
+        }
+    }
+
+    /**
+     * exit from program
+     */
+    public void exit() {
+        System.exit(0);
+    }
+
+    /**
+     * change mode of work
+     * online - get updates online
+     * offline - get result after tests end
+     * @param mode
+     */
+    public void switchMode(int mode) {
+        this.mode = mode;
+    }
+
+    /**
+     * draw plot from file
+     * @throws FileNotFoundException
+     */
+    public void plotFromFile() throws FileNotFoundException {
+        Queue<Long> queue = getDataFromFile();
+
+        Plot.drawConstantPlot(queue);
+    }
+
+
+    /**
+     * check is config null and validate it
+     * @param config
+     * @return
+     */
+    private static boolean checkConfig(MainConfig config) {
+        if (config == null) {
+            return false;
+        }
+
+        return ConfigParser.validateConfig(config);
+    }
+
+    /**
+     * wait end of clients work
+     * @param queue
+     * @param config
+     */
+    private static void waitUntilEnd(Queue<Long> queue, MainConfig config) {
+        int queueSize = queue.size();
+        while (true) {
+            try {
+                Thread.sleep(config.getUpdateTime());
+            } catch (InterruptedException e) {
+                Interpreter.reportAboutError("interrupt main thread");
+            }
+            if (queue.size() == queueSize) {
+                return;
+            }
+            queueSize = queue.size();
+        }
+    }
+
+    /**
+     * save result from client into file
+     * @param queue
+     * @throws IOException
+     */
+    private static void saveInFile(Queue<Long> queue) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_NAME));
+        for (Long time: queue) {
+            writer.write(time.toString() + '\n');
+        }
+
+        writer.close();
+    }
+
+    /**
+     * Read data from file saved by offline mode
+     * @return
+     * @throws FileNotFoundException
+     */
+    private static Queue<Long> getDataFromFile() throws FileNotFoundException {
+        Queue<Long> globalQueue = new ArrayDeque<>();
+        Scanner scanner = new Scanner(new File(FILE_NAME));
+
+        while (scanner.hasNext()) {
+            globalQueue.add(scanner.nextLong());
+        }
+
+        return globalQueue;
+    }
+}
--- TestJabber/src/main/java/Local/UI/Interpreter.java
+++ TestJabber/src/main/java/Local/UI/Interpreter.java
@@ -1,20 +1,12 @@
 package Local.UI;
 
-import Local.Comunicator.ClientCommunicator;
-import Local.Configuration.ConfigParser;
-import Local.Configuration.MainConfig;
+import Local.Controller;
 
-import java.io.*;
-import java.util.ArrayDeque;
-import java.util.Queue;
 import java.util.Scanner;
-import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.logging.Logger;
 
 public class Interpreter {
     private static Logger log = Logger.getLogger(Interpreter.class.getName());
-
-    private static final String FILE_NAME = "result";
 
     private static final String START = "start";
     private static final String LOAD_CONFIG = "load config";
@@ -26,70 +18,54 @@ public class Interpreter {
     private static final String PLOT_FROM_FILE = "plot from file";
 
     public static void main(String[] args) throws Exception {
-        MainConfig config = null;
-
-        ClientCommunicator communicator = new ClientCommunicator();
+        Controller controller = new Controller();
         Scanner in = new Scanner(System.in);
-        int mode = Mode.ONLINE;
 
         while (true) {
             String command = in.nextLine().toLowerCase().trim();
             log.info("Parse  command " + command);
 
-            switch (command) {
-                case START:
-                    if (!checkConfig(config)) {
-                    Interpreter.reportAboutError("wrong config");
-                    }
-                    communicator.startCommunication(config);
-                    break;
+            try {
+                switch (command) {
+                    case START:
+                        controller.start();
+                        break;
 
-                case LOAD_CONFIG:
-                    communicator.sendConfig();
-                    break;
+                    case LOAD_CONFIG:
+                        controller.loadConfig();
+                        break;
 
-                case START_TEST:
-                    Queue<Long> globalQueue = new ConcurrentLinkedQueue<>();
-                    communicator.startTesting(globalQueue);
-                    if (!checkConfig(config)) {
-                        Interpreter.reportAboutError("wrong config");
-                    }
-                    if (mode == Mode.ONLINE) {
-                        Thread ui = new Plot(config, globalQueue);
-                        ui.start();
-                    } else {
-                        waitUntilEnd(globalQueue, config);
-                        saveInFile(globalQueue);
-                        return;
-                    }
-                    break;
+                    case START_TEST:
+                        controller.startTest();
+                        break;
 
-                case PARSE_CONFIG:
-                    config = ConfigParser.readConfig(in);
-                    break;
+                    case PARSE_CONFIG:
+                        controller.parseConfig();
+                        break;
 
-                case EXIT:
-                    System.exit(0);
-                    break;
+                    case EXIT:
+                        controller.exit();
+                        break;
 
-                case MODE_OFFLINE:
-                    mode = Mode.OFFLINE;
-                    break;
+                    case MODE_OFFLINE:
+                        controller.switchMode(Mode.OFFLINE);
+                        break;
 
-                case MODE_ONLINE:
-                    mode = Mode.ONLINE;
-                    break;
+                    case MODE_ONLINE:
+                        controller.switchMode(Mode.ONLINE);
+                        break;
 
-                case PLOT_FROM_FILE:
-                    Queue<Long> queue = getDataFromFile();
+                    case PLOT_FROM_FILE:
+                        controller.plotFromFile();
+                        break;
 
-                    Plot.drawConstantPlot(queue);
-                    break;
+                    default:
+                        System.out.println("Unknown command");
+                        break;
 
-                default:
-                    System.out.println("Unknown command");
-                    break;
-
+                }
+            } catch (Exception e) {
+                reportAboutError(e.getMessage());
             }
         }
     }
@@ -101,68 +77,5 @@ public class Interpreter {
     public static void reportAboutError(String error) {
         System.out.println("Critical error: " + error);
         System.exit(1);
-    }
-
-    /**
-     * wait end of clients work
-     * @param queue
-     * @param config
-     */
-    private static void waitUntilEnd(Queue<Long> queue, MainConfig config) {
-        int queueSize = queue.size();
-        while (true) {
-            try {
-                Thread.sleep(config.getUpdateTime());
-            } catch (InterruptedException e) {
-                Interpreter.reportAboutError("interrupt main thread");
-            }
-            if (queue.size() == queueSize) {
-                return;
-            }
-            queueSize = queue.size();
-        }
-    }
-
-    /**
-     * check is config null and validate it
-     * @param config
-     * @return
-     */
-    private static boolean checkConfig(MainConfig config) {
-        if (config == null) {
-            return false;
-        }
-
-        return ConfigParser.validateConfig(config);
-    }
-
-    /**
-     * save result from client into file
-     * @param queue
-     * @throws IOException
-     */
-    private static void saveInFile(Queue<Long> queue) throws IOException {
-        BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_NAME));
-        for (Long time: queue) {
-            writer.write(time.toString() + '\n');
-        }
-
-        writer.close();
-    }
-
-    /**
-     * Read data from file saved by offline mode
-     * @return
-     * @throws FileNotFoundException
-     */
-    private static Queue<Long> getDataFromFile() throws FileNotFoundException {
-        Queue<Long> globalQueue = new ArrayDeque<>();
-        Scanner scanner = new Scanner(new File(FILE_NAME));
-
-        while (scanner.hasNext()) {
-            globalQueue.add(scanner.nextLong());
-        }
-
-        return globalQueue;
     }
 }
